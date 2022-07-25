4f79772db816d3ab0ce2b283cd8b46b2647a1a15
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-07-25 11:20:07 +0900

    mqtt

--- mirror/KM/stt.js
+++ mirror/KM/stt.js
@@ -44,18 +44,27 @@ const recognizeStream = client
     )
     
   );
+
+var create_memo = 0;
+
 function stt(data){
-
   let value = data.results[0] && data.results[0].alternatives[0]
-  ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+  ? `${data.results[0].alternatives[0].transcript}\n`
   : '\n\nReached transcription time limit, press Ctrl+C\n'
-  if (value.includes("메모")) {
-    mqttClient.publish('memo',value);
-      return `받은 내용: ${value} -> 메모가 포함됨\n`;
-  }
+  if(create_memo == 0) {
+    if (value.includes("메모")) {
+      create_memo = 1;
+      mqttClient.publish('create_memo',"create");
+        return `받은 내용: ${value} -> 메모 호출\n`;
+    }
 
-  mqttClient.publish('memo',value);
-  return `받은 내용: ${value} -> 메모가 아닙니다\n`;
+    return `받은 내용: ${value} -> 메모를 호출하지 않음\n`;
+  }
+  else {
+    create_memo = 0;
+    mqttClient.publish('memo_content',value);
+    return `메모 전달 내용: ${value}\n`;
+  }
 }
 // Start recording and send the microphone input to the Speech API.
 // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
