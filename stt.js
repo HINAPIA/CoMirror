3baa4d039b3d9d9b6bbdd3cfd0a53f2550acde9d
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-09-09 16:37:36 +0900

    stt 전화 명령

--- stt.js
+++ stt.js
@@ -1,4 +1,3 @@
-
 /* Section1. mqtt 모듈 require 및 broker 연동 */
 const mqtt = require('mqtt')
 const options = {  //broker 연동 위한 옵션(브로커 IP 및 포트번호)
@@ -46,40 +45,92 @@ const recognizeStream = client
     process.stdout.write(
       stt(data)
     )
-
   );
 
-var create_memo = 0;
+var create_memo = 0
+let geumBi = true
+let create_message = false
+
+const getFriendName = function (value, keyword) {
+  let callValue = value.split(keyword)
+  let callName = callValue[0].split(" ")
+  return callName[callName.length - 1]
+}
+
+const setGeumbi = () => {
+  //geumBi = false
+  mqttClient.publish('geumBi', 'geumBi_off')
+}
 
 function stt(data) {
   let value = data.results[0] && data.results[0].alternatives[0]
     ? `${data.results[0].alternatives[0].transcript}\n`
     : '\n\nReached transcription time limit, press Ctrl+C\n'
-  if (create_memo == 0) {
-    if (value.includes("전화")) {
-      mqttClient.publish('call_request', "call")
-      return `받은 내용: ${value} -> 전화 호출\n`
-    }
-    else if (value.includes("메모")) {
-      create_memo = 1;
-      //publish('create_memo',"create");
-      mqttClient.publish('create_memo', "create");
-      return `받은 내용: ${value} -> 메모 호출\n`;
-    }
 
-    else if (value.includes("연락처")) {
-      mqttClient.publish('callbook_request', "callbook")
-      return `받은 내용: ${value} ->  호출\n`
+  if (!geumBi) {
+    if (value.includes("금비")) {
+      mqttClient.publish('geumBi', 'geumBi_on')
+      geumBi = true
+      return `받은 내용: ${value} -> 금비 호출\n`
     }
-
-    else if (value.includes("메시지")) {
-      mqttClient.publish('message_request', "message")
-      return `받은 내용: ${value} ->  호출\n`
-    }
-
-    return `받은 내용: ${value} -> 메모를 호출하지 않음\n`;
+    return `받은 내용: ${value} -> 금비 호출 X\n`
   }
   else {
+    let friendName = null
+    if (value.includes("이한테")) {
+      friendName = getFriendName(value, "이한테")
+    }
+    else if (value.includes("이에게")) {
+      friendName = getFriendName(value, "이에게")
+    }
+    else if (value.includes("한테")) {
+      friendName = getFriendName(value, "한테")
+    }
+    else if (value.includes("에게")) {
+      friendName = getFriendName(value, "에게")
+    }
+
+    if (!create_message) {
+      if (value.includes("전화")) {
+        mqttClient.publish('call_request', friendName)
+        setGeumbi()
+        return `받은 내용: ${value} -> 전화 호출\n`
+      }
+      else if (value.includes("영상 통화") || value.includes("영상통화")) {
+        mqttClient.publish('video_call_request', friendName)
+        setGeumbi()
+        return `받은 내용: ${value} -> 영상통화 호출\n`
+      }
+
+      else if (value.includes("메시지") || value.includes("메세지")) {
+        mqttClient.publish('message_request', friendName)
+        return `받은 내용: ${value} -> 메시지 호출\n`
+      }
+
+      else if (value.includes("메모")) {
+        create_memo = 1;
+        //publish('create_memo',"create");
+        mqttClient.publish('create_memo', friendName);
+        setGeumbi()
+        return `받은 내용: ${value} -> 메모 호출\n`;
+      }
+
+      else if (value.includes("연락처")) {
+        mqttClient.publish('callbook_request', friendName)
+        setGeumbi()
+        return `받은 내용: ${value} ->  호출\n`
+      }
+
+      setGeumbi()
+      return `받은 내용: ${value}\n`;
+    }
+    else if(create_message) {
+      setGeumbi()
+      mqttClient.publish('message_content', value);
+      return `메시지 전달 내용: ${value}\n`;
+    }
+  }
+  if (create_memo == 1) {
     create_memo = 0;
     //publish('memo_content',value);
     mqttClient.publish('memo_content', value);
