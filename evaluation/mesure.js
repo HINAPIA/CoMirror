a603fb3e4bf6b386f77f2b204d0d64463cefa70e
syy0O <86238720+syy0O@users.noreply.github.com>
2022-12-29 10:47:28 +0900

    add mesure class

---
+++ evaluation/mesure.js
@@ -0,0 +1,85 @@
+const writeXlsxFile = require("write-excel-file/node");
+const fs = require("fs");
+
+class Mesuare {
+    
+    constructor(cnt){
+        this.TEST_COUNT = cnt
+        this.departure_time = []
+        this.arrival_time = []
+    }
+
+    putDepartureTime(time) {
+        departure_time.push(time);
+    }
+    putArrivalTime(time) {
+        arrival_time.push(time);
+    }
+
+    write() {
+
+        const HEADER_ROW = [
+            {
+              value: "//",
+              fontWeight: "bold",
+            },
+            {
+              value: "전송 시간",
+              fontWeight: "bold",
+            },
+            {
+              value: "수신 시간",
+              fontWeight: "bold",
+            },
+            {
+              value: "걸린 시간",
+              fontWeight: "bold",
+            },
+          ];
+          
+          let DATA_ROWS = []
+          for (let i = 0; i< this.TEST_COUNT; i++){
+            let  DATA_ROW = [
+                {
+                    type:Date,
+                    value: new Date(),
+                    format:"yyyy/mm/dd"
+                    // value:this.departure_time[i]
+                },
+                {
+                    type:Date,
+                    value: new Date(),
+                    format:"yyyy/mm/dd"
+                    // type:Date,
+                    // value:this.arrival_time[i]
+
+                },
+                {
+                    type:Number,
+                    value:1
+                    // value:this.arrival_time[i] - this.departure_time[i]
+                }
+            ]
+
+            DATA_ROWS.push(DATA_ROW)
+          }
+         
+         const data = [HEADER_ROW, ...DATA_ROWS]
+
+         const makeExcel = async () => {
+            if (!fs.existsSync("./excel")) {
+              // excel 폴더가 존재하지 않는 경우 excel 폴더를 생성한다.
+              fs.mkdirSync("./excel");
+            }
+            await writeXlsxFile(data, {
+              filePath: "./excel/member.xlsx",
+            });
+          };
+          
+          makeExcel();
+    }
+    
+};
+
+const mesure = new Mesuare(10)
+mesure.write()
